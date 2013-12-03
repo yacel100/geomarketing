17bc1d7ea699290e96ac1b9efb20fa3bd3e6c574
yacel100 <master.yac@gmail.com>
2013-12-03 17:48:12 -0400

    Introducciendo un market en el mapa

--- Geomarketing/src/yac/tesis/geomarketing/MainActivity.java
+++ Geomarketing/src/yac/tesis/geomarketing/MainActivity.java
@@ -1,15 +1,28 @@
 package yac.tesis.geomarketing;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	
+	private GoogleMap map;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        map.addMarker(new MarkerOptions().position(new LatLng(-17.391991,-66.153271)).title("aca estoy"));
+        
     }
 
 
