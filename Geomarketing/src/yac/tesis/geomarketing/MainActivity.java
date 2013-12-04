ec2827dd9473b739c5c8474a7a7874d5aa9df31c
yacel100 <master.yac@gmail.com>
2013-12-03 23:03:50 -0400

    realizando los market, mostrando los iconos y mensajes de la ubicaciones definidas

--- Geomarketing/src/yac/tesis/geomarketing/MainActivity.java
+++ Geomarketing/src/yac/tesis/geomarketing/MainActivity.java
@@ -1,19 +1,25 @@
 package yac.tesis.geomarketing;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	
 	private GoogleMap map;
-	
+	private static final LatLng MELBOURNE = new LatLng(-37.81319, 144.9629);
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,23 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
         
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        
+        //creando la variable market el cual mostrar la marca en el mapa
+        Marker melboure = map.addMarker(new MarkerOptions().position(MELBOURNE).title("melbourne").snippet("Poblacion 4.000.000").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+        
+        //Agregando un market en el mapa de la aplicacion
         map.addMarker(new MarkerOptions().position(new LatLng(-17.391991,-66.153271)).title("aca estoy"));
+        
+        //Agregando un mensaje de fondo negro que indica el title del market en donde pinchamos
+        map.setOnMarkerClickListener(new OnMarkerClickListener() {
+			
+			@Override
+			public boolean onMarkerClick(Marker arg0) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplicationContext(), arg0.getTitle(),Toast.LENGTH_LONG ).show();
+				return false;
+			}
+		});
         
     }
 
