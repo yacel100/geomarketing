9f2f83924d8dcc63b259394b338e990f87743997
yacel100 <master.yac@gmail.com>
2013-12-04 02:16:44 -0400

    realizando la manipulacion con la base de datos

---
+++ sqlite/src/com/yac/sqlite/MainActivity.java
@@ -0,0 +1,17 @@
+package com.yac.sqlite;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+}
